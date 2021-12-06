bcf77bcd5736161123409d827685a73be1f21a47
wilas0o0o <03mcu30@gmail.com>
2021-12-06 05:13:47 +0000

    [Add]comments#create,destroy

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :book
   belongs_to :user
   
-  validates_uniquness_of :book_id,scope: :user_id
+  validates_uniqueness_of :book_id,scope: :user_id
 end
