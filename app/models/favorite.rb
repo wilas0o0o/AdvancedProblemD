dda9b1f7018a9112814912b2c68ea44c69a2adad
wilas0o0o <03mcu30@gmail.com>
2021-12-06 01:27:12 +0000

    [Add]BookComment

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :book
   belongs_to :user
   
-
+  validates_uniquness_of :book_id,scope: :user_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_115608) do
+ActiveRecord::Schema.define(version: 2021_12_06_011740) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
