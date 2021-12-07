279a39e6b602cf67e76eb6c3afd24495d18fc1f6
wilas0o0o <03mcu30@gmail.com>
2021-12-07 01:11:17 +0000

    [Start]応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  devise_for :users
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships,only: [:create,:destroy]
-    get "followings" => "relationships#followings",as: "followings"
-    get "followers" => "relationships#followers",as: "followers"
+    get 'followings' => 'relationships#followings',as: 'followings'
+    get 'followers' => 'relationships#followers',as: 'followers'
   end
   resources :books do
     resource :favorites,only: [:create,:destroy]
