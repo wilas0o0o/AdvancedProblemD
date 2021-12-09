77565c8380478b56a3c4d495dd1608a382317332
wilas0o0o <03mcu30@gmail.com>
2021-12-09 12:52:46 +0000

    [Update]Ajax_favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,11 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
