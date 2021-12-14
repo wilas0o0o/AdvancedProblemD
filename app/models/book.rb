c6675b2b19bcded74d45ce93031a0fb941ad116f
wilas0o0o <03mcu30@gmail.com>
2021-12-14 00:43:12 +0000

    [Finish]応用課題6

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,12 +5,12 @@ class Book < ApplicationRecord
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
-	
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
-  def self.search_for(content, method)
+
+  def self.search_for(method, content)
     if method == "perfect"
       Book.where(title: content)
     elsif method == "forward"
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,22 +12,21 @@ class User < ApplicationRecord
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
-	#フォローされているユーザーのリレーション
-	has_many :passive_relationships,class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
-	#Relationshipテーブルのfollowed_id(自分がフォローされているユーザーのid)と関連付ける
-	
-	#フォローしているユーザーのリレーション
+  #フォローしているユーザーのリレーション
 	has_many :active_relationships,class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
 	#Relationshipテーブルのfollower_id(自分がフォローしているユーザのid)と関連付ける
-	
-  #自分がフォローされているユーザーの一覧画面で使う
-  has_many :followers,through: :passive_relationships,source: :follower
-  #passive_relationshipsを経由してfollowerを持ってくる
   
   #自分がフォローしているユーザーの一覧画面で使う
   has_many :followings,through: :active_relationships,source: :followed
   #active_relationshipsを経由してfollowedを持ってくる
 
+	#フォローされているユーザーのリレーション
+	has_many :passive_relationships,class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
+	#Relationshipテーブルのfollowed_id(自分がフォローされているユーザーのid)と関連付ける
+	
+	#自分がフォローされているユーザーの一覧画面で使う
+  has_many :followers,through: :passive_relationships,source: :follower
+  #passive_relationshipsを経由してfollowerを持ってくる
   
   def follow(user_id)
     active_relationships.create(followed_id: user_id)
@@ -44,9 +43,9 @@ class User < ApplicationRecord
   def self.search_for(method, content)
     if method == "perfect"
       User.where(name: content)
-    elsif method == "forward_match"
+    elsif method == "forward"
       User.where("name LIKE ?",content+"%" )
-    elsif method == "backward_match"
+    elsif method == "backward"
       User.where("name LIKE ?","%"+content )
     elsif method == "partial"
       User.where("name LIKE ?","%"+content+"%" )
